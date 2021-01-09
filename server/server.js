457bc8292c3f5c152279c54173057ee7bd76524f
bdzanko17 <bdzanko1@gmail.com>
2021-01-09 14:54:56 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.use(express.static(__dirname + '/client'))
+
+io.on('connection', (socket) => {
+    console.log('a user is connected')
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+      });
+
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+})
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
